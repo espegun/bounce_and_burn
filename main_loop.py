201ec71ad22b3da72fd4f2afbb679e229114e00a
Espen Gunnarsen <espen.gunnarsen@gmail.com>
2020-09-10 20:39:55 +0200

    Update

--- main_loop.py
+++ main_loop.py
@@ -8,8 +8,8 @@ from Visualizer import Visualizer
 print("Welcome to the amazinglicious game!")
 
 game = Game()
-game.add_Ball(Ball("TestBall1", 10, 100, 100, (255, 0, 0)))
-game.add_Ball(Ball("TestBall2", 10, 200, 100, (0, 0, 255)))
+game.add_Ball(Ball("Red", 10, 100, 100, (255, 0, 0)))
+game.add_Ball(Ball("Blue", 10, 200, 100, (0, 0, 255)))
 for b in game.get_Balls():
     b.set_speed(-10, 10)
 
@@ -24,7 +24,6 @@ pygame.display.set_caption("Bounce and burn")
 WHITE = (255, 255, 255)
 BLUE = (0, 0, 255)
 
-
 while True:  # the main game loop
 
     surface.fill(WHITE)
@@ -38,10 +37,7 @@ while True:  # the main game loop
             sys.exit()
 
     for b in game.get_Balls():
-        #print (b.get_pos())
         b.update_pos(1/FPS)
-        #print (b.get_pos())
-        #sys.exit(1)
 
     for b in game.get_Balls():
         pos = b.get_pos()
